6a016d260e086146941d025694f6d11df8324845
nqh <ptitnqh@gmail.com>
2022-05-14 23:39:48 +0700

    fixed 14.05

---
+++ Demo/NumberButton.swift
@@ -0,0 +1,20 @@
+//
+//  NumberButton.swift
+//  Demo
+//
+//  Created by Nguyen Quang Huy on 14/05/2022.
+//
+
+import UIKit
+
+class NumberButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
