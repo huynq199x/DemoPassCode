d5d90e3caf970c35bda81981a3519cbfe2cc9e9b
nqh <ptitnqh@gmail.com>
2022-05-14 23:42:30 +0700

    fixed 14.05

--- Demo/NumberButton.swift
+++ Demo/NumberButton.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class NumberButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        actions()
     }
-    */
 
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        actions()
+    }
+    
+    private func actions() {
+        addTarget(self, action: #selector(handleTouchDown), for: .touchDown)
+        addTarget(self, action: #selector(handleTouchUp), for: .touchUpInside)
+    }
+    
+    @objc func handleTouchDown() {
+        var rgb: (red: CGFloat, green: CGFloat, blue: CGFloat) = (1, 1, 1)
+        tintColor.getRed(&rgb.red, green: &rgb.green, blue: &rgb.blue, alpha: nil)
+        animate(back: .darkGray)
+    }
+    
+    @objc func handleTouchUp() {
+        animate(back: .black)
+    }
+    
+    private func animate(back: UIColor?) {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
+            self.backgroundColor = back
+        })
+    }
 }
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,10 +6,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressCenterConstraint: NSLayoutConstraint!
   
     @IBOutlet weak var progressView: UIView!
-    @IBOutlet var buttons: [UIButton]!
     
-    @IBOutlet var passCodeCharacter: [PassCodeCharacter]!
-  
+    @IBOutlet var passCodeCharacter: [UIView]!
+    
     @IBOutlet weak var stackViewPassCodeCharacter: UIStackView!
     
     private let animationDuration: CGFloat = 0.33
@@ -18,70 +17,74 @@ class ViewController: UIViewController {
     private let pass: String = "0000"
     private var spacingOfStackViewPassCodeCharacter: CGFloat!
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    for button in buttons {
-        button.backgroundColor = .black
-        button.tintColor = .white
+    override func viewDidLoad() {
+        super.viewDidLoad()
         spacingOfStackViewPassCodeCharacter = stackViewPassCodeCharacter.spacing
+        for character in passCodeCharacter {
+            character.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            character.layer.borderWidth = 1
+            character.layer.cornerRadius = 15
+            character.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
-  }
-    
-  @IBAction func buttonPressed(_ sender: UIButton) {
-    input += String(sender.tag)
-      self.progressWidthConstraint.constant = 240 * min(CGFloat(self.input.count)/CGFloat(numberOfPasswordCharacter), 1) + spacingOfStackViewPassCodeCharacter * CGFloat(self.input.count - 1)
-    progressView.layer.cornerRadius = 30
-    
-    UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseInOut) {
-      self.view.layoutIfNeeded()
-    } completion: { _ in
-      if self.input.count == self.numberOfPasswordCharacter {
-        if self.input == self.pass {
 
-          self.progressWidthConstraint.constant = CGFloat(240 / self.numberOfPasswordCharacter)
-          self.progressLeadingConstraint.isActive = false
-          self.progressCenterConstraint.isActive = true
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        input += String(sender.tag)
+        self.progressWidthConstraint.constant = 120 * min(CGFloat(self.input.count)/CGFloat(numberOfPasswordCharacter), 1) + spacingOfStackViewPassCodeCharacter * CGFloat(self.input.count - 1)
+        progressView.layer.cornerRadius = 15
+        
+        UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseInOut) {
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            if self.input.count == self.numberOfPasswordCharacter {
+                if self.input == self.pass {
+
+                    self.progressWidthConstraint.constant = CGFloat(120 / self.numberOfPasswordCharacter)
+                    self.progressLeadingConstraint.isActive = false
+                    self.progressCenterConstraint.isActive = true
           
-          UIView.animate(withDuration: self.animationDuration, delay: 0.5, options: .curveEaseInOut) {
-            self.view.layoutIfNeeded()
-              self.stackViewPassCodeCharacter.isHidden = true
-            self.progressView.backgroundColor = .green
-          } completion: { _ in
-            // Reset
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-              self.showUnlockView()
+                    UIView.animate(withDuration: self.animationDuration, delay: 0.5, options: .curveEaseInOut) {
+                        self.view.layoutIfNeeded()
+                        self.stackViewPassCodeCharacter.isHidden = true
+                        self.progressView.backgroundColor = .green
+                    } completion: { _ in
+                        // Reset
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            self.showUnlockView()
+                        }
+                    }
+                }
+                else {
+                    UIView.animate(withDuration: self.animationDuration, delay: 0.5, options: .curveEaseInOut) {
+                        self.view.layoutIfNeeded()
+                    } completion: { _ in
+                        // Reset
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            self.input = ""
+                            self.progressWidthConstraint.constant = 0
+                            self.progressCenterConstraint.isActive = false
+                            self.progressLeadingConstraint.isActive = true
+                        }
+                    }
+                }
             }
-          }
         }
-        else {
-          UIView.animate(withDuration: self.animationDuration, delay: 0.5, options: .curveEaseInOut) {
+    }
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+        if self.input.count > 0 {
+            self.input.removeLast()
+        }
+        self.progressWidthConstraint.constant = 120 * min(CGFloat(self.input.count)/CGFloat(numberOfPasswordCharacter), 1) + spacingOfStackViewPassCodeCharacter * CGFloat(self.input.count - 1)
+        UIView.animate(withDuration: self.animationDuration, delay: 0.05, options: .curveEaseInOut){
             self.view.layoutIfNeeded()
-          } completion: { _ in
-            // Reset
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            self.input = ""
-            self.progressWidthConstraint.constant = 0
-            self.progressCenterConstraint.isActive = false
-            self.progressLeadingConstraint.isActive = true
-            }
-          }
         }
-      }
     }
-  }
-  @IBAction func deleteButtonPressed(_ sender: Any) {
-    if self.input.count > 0 {
-      self.input.removeLast()
+    
+    @objc func showUnlockView(){
+        let unlockVC = UIStoryboard(name: "Main", bundle: nil)
+        let controller = unlockVC.instantiateViewController(withIdentifier: "UnlockViewController") as! UnlockViewController
+        let navigation = UINavigationController(rootViewController: controller)
+        self.view.addSubview(navigation.view)
     }
-    self.progressWidthConstraint.constant = 240 * min(CGFloat(self.input.count)/CGFloat(numberOfPasswordCharacter), 1) + spacingOfStackViewPassCodeCharacter * CGFloat(self.input.count - 1)
-    UIView.animate(withDuration: self.animationDuration, delay: 0.05, options: .curveEaseInOut){
-      self.view.layoutIfNeeded()
-    }
-  }
-  @objc func showUnlockView(){
-    let unlockVC = UIStoryboard(name: "Main", bundle: nil)
-    let controller = unlockVC.instantiateViewController(withIdentifier: "UnlockViewController") as! UnlockViewController
-    let navigation = UINavigationController(rootViewController: controller)
-    self.view.addSubview(navigation.view)
-  }
 }
+
